c1b9e5a39c50078619e37d67de9cd29f6c1aa577
sototh <sad@daw>
2025-11-15 15:58:13 +0800

    comms

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ import cv2
 from Detection import KafkaMessenger
 
 if __name__ == "__main__":
+    # on stand
     # cap = cv2.VideoCapture(0)
+    
+    # debug
     image = cv2.imread('./real_caklib.png')
     messenger = KafkaMessenger(['localhost:8000'], image)
 
     while True:
+        # on stand
         # _,image = cap.read()
         messenger.send_data(image)
