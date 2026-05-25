38113d99827f9d3081271ad9fc689af487c57553
sototh <sad@daw>
2026-05-25 16:17:58 +0800

    config

--- src/Config/config.py
+++ src/Config/config.py
@@ -8,7 +8,11 @@ STAND_ID = "hw_stand_001"
 PIN_CODE = "1234"  # Пин-код для привязки в Dashboard лобби
 
 # --- Настройки Pygame ---
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 640
 FPS = 30
-SQUARE_SIZE = SCREEN_WIDTH // 8
+SQUARE_SIZE = 75
+
+CELL_PADDING = 5      # Размер черного отступа вокруг клетки (в пикселях)
+INDICATOR_WIDTH = 2   # Ширина индикатора в количестве столбцов
+# Пересчитываем ширину экрана: 8 клеток + 2 клетки индикатора
+SCREEN_WIDTH = (8 + INDICATOR_WIDTH) * SQUARE_SIZE 
+SCREEN_HEIGHT = 8 * SQUARE_SIZE
